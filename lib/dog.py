f84cc54026c79d933d21264d0a2155f7fd000c6c
mmwandera <mmwandera74@gmail.com>
2024-01-14 13:18:52 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,18 @@
 
 class Dog:
     # Class body goes here
+    def bark(self):
+        print("Woof!")
 
-    #Instance method definition
-    pass
+    # Add an instance method sit() to your Dog class that will print "The dog is sitting."
+    def sit(self):
+        print("The dog is sitting.")
+
+#Instance method definition
+fido = Dog()
+fido.bark()
+
+
+snoopy = Dog()
+snoopy.bark()
+
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
+# Define a Person class in lib/person.py
 class Person:
     # Class body goes here
 
-    #Instance method definition
-    pass
+    # Add an instance method talk() to your Person class that will print "Hello World!"
+    def talk(self):
+        print("Hello World!")
+
+    # Add an instance method walk() to your Person class that will print "The person is walking."
+    def walk(self):
+        print("The person is walking.")
+
